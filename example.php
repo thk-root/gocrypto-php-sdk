73443e2c1ce534ec5e5e2b064f06a470e9b515a0
Tilen HK <tilen.enteres@eu>
2020-09-28 22:23:32 +0200

    Updated examples

--- example.php
+++ example.php
@@ -1,14 +1,14 @@
 <?php
 
+	use GoCrypto\SDK\GoCrypto;
 	use GoCrypto\SDK\GoCryptoStaging;
 
 	require_once __DIR__ . '/src/GoCrypto.class.php';
-	require_once __DIR__ . '/src/GoCryptoStaging.class.php';
 
 	$gc = new GoCryptoStaging('GoCrypto SDK staging test', 'https://yourshop.com/success');
 
 	//OR, for production purposes:
-	//$gc = new GoCrypto('GoCrypto SDK staging test', 'c83aa8ac-b29a-41a6-b039-3ed5ac69acff', 'xzOOGVskFG2IeV2W3SFFFbEPC8vMDflz2k6LjrNXDCM4BFvH', false, 'GoCrypto test shop', 'https://yourshop.com/success', 'https://yourshop.com/cancel');
+	$gc = new GoCrypto('c83aa8ac-b29a-41a6-b039-3ed5ac69acff', 'xzOOGVskFG2IeV2W3SFFFbEPC8vMDflz2k6LjrNXDCM4BFvH', false, 'GoCrypto test shop', 'https://yourshop.com/success', 'https://yourshop.com/cancel');
 
 	//Adding items
 	$gc->addItem('Test product name 1', 'Test product description 1', 1, 10.99);
